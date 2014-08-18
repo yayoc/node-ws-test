36f81986f3f599a80af6bc5b1cb8518e5afef774
yayoc <n.yayoshi+github@gmail.com>
2014-08-18 21:28:25 +0900

    fixed

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-var server = http.createServer(app);
-server.listen(port);
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-var wss = new WebSocketServer({server: server});
-console.log('websocket server created');
-wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-          ws.send(JSON.stringify(new Date()), function() {  });
-            }, 1000);
+app.use(express.static(__dirname + "/"))
 
-      console.log('websocket connection open');
+var server = http.createServer(app)
+server.listen(port)
 
-        ws.on('close', function() {
-              console.log('websocket connection close');
-                  clearInterval(id);
-                    });
-});
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
